e5ef387fd8a049466241628fa4db77e512c5ee65
k223kim <k223kim@uwaterloo.ca>
2024-07-29 18:48:00 +0900

    Quartz sync: Jul 29, 2024, 6:48 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Kaeun Kim",
+    pageTitle: "🪴 Hi Kaeun Kim",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
